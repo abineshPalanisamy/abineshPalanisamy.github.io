b0d3dbe8ce537960c8bac815328e48bd9219d759
abineshPalanisamy <abinesh.palanisamy@syncfusion.com>
2025-04-03 20:24:55 +0530

    changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2880381328"
+    serviceWorkerVersion: "3438625921"
   }
 });
